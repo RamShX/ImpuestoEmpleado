8015e771973be000d0b88ea7a04bfb8a5e7f4b2c
Ramshley P. <ramshleyv@gmail.com>
2025-03-24 15:30:35 -0400

    agregación: factories de empleado y contexts de empleado

---
+++ ImpuestoEmpleado.Domain/Contexts/ContextEmpleado.cs
@@ -0,0 +1,23 @@
+﻿
+
+using ImpuestoEmpleado.Domain.Interfeces;
+
+namespace ImpuestoEmpleado.Domain.Contexts
+{
+    public class ContextEmpleado
+    {
+        private IEmpleado _empleado;
+
+        public ContextEmpleado(IEmpleado empleado)
+        {
+            _empleado = empleado;
+        }
+
+        public decimal CalcularImpuesto(decimal salario)
+        {
+            return _empleado.CalcularImpuesto(salario);
+        }
+
+
+    }
+}
---
+++ ImpuestoEmpleado.Domain/Factories/FactoryEmpleado.cs
@@ -0,0 +1,25 @@
+﻿
+
+using ImpuestoEmpleado.Domain.Interfeces;
+using ImpuestoEmpleado.Domain.Models;
+
+namespace ImpuestoEmpleado.Domain.Factories
+{
+    public class FactoryEmpleado
+    {
+        public static IEmpleado Create(string nombre, decimal salario, int tipo)
+        {
+            switch (tipo)
+            {
+                case 1:
+                    return new EmpleadoTiempoCompleto(nombre, salario);
+                case 2:
+                    return new EmpleadoMedioTiempo(nombre, salario);
+                case 3:
+                    return new EmpleadoPorContracto(nombre, salario);
+                default:
+                    throw new ArgumentException("Tipo de empleado no válido");
+            }
+        }
+    }
+}
