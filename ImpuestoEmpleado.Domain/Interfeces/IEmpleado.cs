1f8bf3a559521058aadb1d330081343821b58b88
Ramshley P. <ramshleyv@gmail.com>
2025-03-24 15:46:14 -0400

    agregación: ManagerEmpleado para controlar la agregacíón de empleado. modificación: se le puso la propiedad TipoEmpleado a la Interfaz

--- ImpuestoEmpleado.Domain/Interfeces/IEmpleado.cs
+++ ImpuestoEmpleado.Domain/Interfeces/IEmpleado.cs
@@ -7,6 +7,7 @@ namespace ImpuestoEmpleado.Domain.Interfeces
         string Nombre { get; set; }
         decimal Salario { get; set; }
         decimal Impuesto { get; set; }
+        string TipoEmpleado { get; }
 
         decimal CalcularImpuesto(decimal salario);
     }
---
+++ ImpuestoEmpleado.Domain/Manager/EmpleadoManager.cs
@@ -0,0 +1,25 @@
+﻿
+using ImpuestoEmpleado.Domain.Interfeces;
+
+namespace ImpuestoEmpleado.Domain.Manager
+{
+    public class EmpleadoManager
+    {
+        public List<IEmpleado> Empleados { get; set; } = new List<IEmpleado>();
+
+        public void AddEmpleado(IEmpleado empleado)
+        {
+            Empleados.Add(empleado);
+            Console.WriteLine("Se agregó completamente el empleado");
+        }
+
+        public void MostrarEmpleado() 
+        {
+            foreach (var e in Empleados)
+            {
+                decimal impuesto = e.CalcularImpuesto(e.Salario);
+                Console.WriteLine($"Nombre: {e.Nombre} | Tipo de empleado: {e.TipoEmpleado} | Salario: {e.Salario} | Impuesto: {e.CalcularImpuesto(impuesto)}");
+            }
+        }
+    }
+}
