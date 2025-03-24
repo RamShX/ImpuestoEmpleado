1985db1c1072c821dbc98f8585f2299103288725
Ramshley P. <ramshleyv@gmail.com>
2025-03-24 18:18:22 -0400

    Implementacón: Se puso a lógica de guardado en TXT

--- ImpuestoEmpleado.Domain/SaveStrategy/StrategyTxt.cs
+++ ImpuestoEmpleado.Domain/SaveStrategy/StrategyTxt.cs
@@ -4,9 +4,17 @@ namespace ImpuestoEmpleado.Domain.SaveStrategy
 {
     class StrategyTxt : IExportable
     {
+        private readonly string _path = "C:\\Users\\Lenovo\\source\\repos\\P2\\Ejercicio Strategy2\\ImpuestoEmpleado\\ImpuestoEmpleado.Domain\\ArchivoGuardado\\Empleado.txt";
         public void Exportar(List<IEmpleado> empleados)
         {
-            
+            string txt = "Nombre | Tipo de empleado | Salario | Impuesto";
+            foreach (var e in empleados)
+            {
+                txt += $"\n{e.Nombre} | {e.TipoEmpleado} | {e.Salario} | {e.CalcularImpuesto(e.Salario)}";
+            }
+
+            File.WriteAllText(_path, txt);
+            Console.WriteLine("Se guardadó los datos en extensión TXT correctamente");
         }
     }
 }
