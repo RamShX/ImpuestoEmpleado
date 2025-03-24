4a0770624d462c8426f4a521957d97f2d1de5552
Ramshley P. <ramshleyv@gmail.com>
2025-03-24 18:16:32 -0400

    Implementación: Se puso la lógica de guadado en JSon

--- ImpuestoEmpleado.Domain/SaveStrategy/StrategyJson.cs
+++ ImpuestoEmpleado.Domain/SaveStrategy/StrategyJson.cs
@@ -1,12 +1,17 @@
 ﻿using ImpuestoEmpleado.Domain.Interfeces;
+using System.Text.Json;
 
 namespace ImpuestoEmpleado.Domain.SaveStrategy
 {
     class StrategyJson : IExportable
     {
+        private readonly string _path = "C:\\Users\\Lenovo\\source\\repos\\P2\\Ejercicio Strategy2\\ImpuestoEmpleado\\ImpuestoEmpleado.Domain\\ArchivoGuardado\\Empleado.json";
         public void Exportar(List<IEmpleado> empleados)
         {
-            throw new NotImplementedException();
+            var json = JsonSerializer.Serialize(empleados, new JsonSerializerOptions{ WriteIndented = true});
+            File.WriteAllText(_path, json);
+            Console.WriteLine("Se guardadó los datos en extensión json correctamente");
+
         }
     }
 }
