3ce935352b65835c60d3245fe5d7b6843ad2f40a
Ramshley P. <ramshleyv@gmail.com>
2025-03-24 18:45:40 -0400

    Agregación: Se implementó el factory de archivo para automatizar la creación de este.

---
+++ ImpuestoEmpleado.Domain/Factories/FactoryArchivo.cs
@@ -0,0 +1,23 @@
+﻿using ImpuestoEmpleado.Domain.Interfeces;
+using ImpuestoEmpleado.Domain.SaveStrategy;
+
+namespace ImpuestoEmpleado.Domain.Factories
+{
+    public class FactoryArchivo 
+    {
+        public IExportable CrearExportable(string tipo)
+        {
+            switch (tipo)
+            {
+                case "Excel":
+                    return new StrategyExcel();
+                case "Txt":
+                    return new StrategyTxt();
+                case "Json":
+                    return new StrategyJson();
+                default:
+                    return null;
+            }
+        }
+    }
+}
